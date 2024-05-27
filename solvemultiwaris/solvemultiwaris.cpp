2269ca530948d665d3814ad76b07f19d68142eb0
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2024-05-27 10:52:29 +0700

    menambahkan kelas budi sebagai turunan dari pekerja dan pelajar

--- solvemultiwaris/solvemultiwaris.cpp
+++ solvemultiwaris/solvemultiwaris.cpp
@@ -32,5 +32,16 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
 
 
