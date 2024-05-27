7950092dd711947bd9d1ba2f13ce5443519e7dcb
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2024-05-27 10:31:24 +0700

    menambahkan kelas baseclass dengan metode virtual perkenalan

--- nooverriding/nooverriding.cpp
+++ nooverriding/nooverriding.cpp
@@ -7,15 +7,4 @@ public:
 		cout << "Hallo saya Function dari derived Class";
 	}
 };
-class derivedClass : public baseClass {
-public:
-	void perkenalan() {
-		cout << "Hallo saya Function dari derived Class";
-	}
-};
-int main() {
-	derivedClass a;
-	a.perkenalan();
 
-	return 0;
-}
